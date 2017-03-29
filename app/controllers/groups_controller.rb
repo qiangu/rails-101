ed207a03b91d30a8636f081ea43c5590c1902c2a
刘传 <yangtuolc@gmail.com>
2017-03-29 22:56:07 +0800

    add permission check on show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,26 +1,28 @@
 class GroupsController < ApplicationController
    before_action :authenticate_user! , only: [:new, :create,:edit, :update, :destroy]
    before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
-   @groups = Group.all
- end
+
+   def index
+     @groups = Group.all
+   end
 
   def show
     @group = Group.find(params[:id])
+
 
      if current_user != @group.user
 
      end
   end
 
- def edit
+  def edit
+  end
 
- end
-
-
- def new
+  def new
    @group = Group.new
- end
- def create
+  end
+
+  def create
     @group = Group.new(group_params)
     @group.user = current_user
 
@@ -41,24 +43,21 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-
-    @group.destroy
-    flash[:alert] = "Group deleted"
-    redirect_to groups_path
+      @group.destroy
+      redirect_to groups_path, alert: "Group deleted"
   end
 
   private
 
   def find_group_and_check_permission
-  @group = Group.find(params[:id])
+       @group = Group.find(params[:id])
 
-  if current_user != @group.user
-    redirect_to root_path, alert: "You have no permission."
-  end
-end
-
-  def group_params
-    params.require(:group).permit(:title, :description)
+       if current_user != @group.user
+         redirect_to root_path, alert: "You have no permission."
+       end
   end
 
-end
+     def group_params
+       params.require(:group).permit(:title, :description)
+     end
+  end
